bfb54993c1022be6e62349f552edf9787cf1ccac
ironboy98 <52446747+ironboy98@users.noreply.github.com>
2019-07-05 09:55:50 +0530

    Update consonat.py

--- consonat.py
+++ consonat.py
@@ -1,15 +1,15 @@
 z=input()
 if(z=='A' or z=='a'):
-  print('it is a Vowel')
+  print('Vowel')
 elif(z=='E' or z=='e'):
-  print('it is a Vowel')
+  print('Vowel')
 elif(z=='I' or z=='i'):
-  print('it is a Vowel')
+  print('Vowel')
 elif(z=='O' or z=='o'):
-  print('it is a Vowel')
+  print('Vowel')
 elif(z=='U' or z=='u'):
-  print('it is a Vowel')
+  print('Vowel')
 elif(z>='a' and z<='z') or (z>='A' and z<='Z'):
-  print('it is a Consonat')
+  print('Consonant')
 else:
   print('invalid')
