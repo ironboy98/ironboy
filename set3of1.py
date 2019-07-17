daa9306848084b7f7188b3bf75f4b8b9f0e61393
ironboy98 <52446747+ironboy98@users.noreply.github.com>
2019-07-17 13:46:34 +0530

    Update set3of1.py

--- set3of1.py
+++ set3of1.py
@@ -3,7 +3,7 @@ ds=0
 lst=input().split()
 lst=list(map(int,lst))
 new=[]
-for i in range(0,sh):
+for i in range(0,hb):
     if((lst.count(lst[i]))>=2):
       if lst[i] not in new:
         new.append(lst[i])
